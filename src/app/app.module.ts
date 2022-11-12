f997a0a93f39b4f573e5d2163f6f8c8fdd1a1a43
lamba <programmer.lamba@gmail.com>
2022-11-12 02:52:10 +0200

    added table with books content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import {MatIconModule} from "@angular/material/icon";
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { BooksComponent } from './books/books.component';
 import {RouterOutlet} from "@angular/router";
+import {MatTableModule} from "@angular/material/table";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavBarComponent,
-    BooksComponent
+    BooksComponent,
   ],
 	imports: [
 		BrowserModule,
@@ -23,6 +25,8 @@ import {RouterOutlet} from "@angular/router";
 		BrowserAnimationsModule,
 		MatIconModule,
 		RouterOutlet,
+		MatTableModule,
+    HttpClientModule
 	],
   providers: [],
   bootstrap: [AppComponent]
